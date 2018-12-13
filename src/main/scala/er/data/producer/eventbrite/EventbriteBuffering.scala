2111165c53d1a9c391147240f136fed9e56e70be
Oleksandr Siianko <oleksandr.siyanko@gmail.com>
2018-12-13 11:56:05 +0100

    switch eventbrite to stream data

--- src/main/scala/er/data/producer/eventbrite/EventbriteBuffering.scala
+++ src/main/scala/er/data/producer/eventbrite/EventbriteBuffering.scala
@@ -3,26 +3,24 @@ package er.data.producer.eventbrite
 import cats.effect.ConcurrentEffect
 import er.data.producer.Logger
 import er.data.producer.eventbrite.EventbriteService._
-import fs2.concurrent.Queue
 import fs2.Stream
-import cats.implicits._
+import fs2.concurrent.Queue
 
 class EventbriteBuffering[F[_]](q: Queue[F, EventbriteEvent], callF: F[EventbriteResponse])
                                (implicit L: Logger[F], C: ConcurrentEffect[F]) {
 
-  def start: F[Unit] = {
+  def start: Stream[F, Unit] = {
 
-    val loop: EventbriteResponse => F[Unit] = _ match {
+    val loop: EventbriteResponse => Stream[F, Unit] = _ match {
       case SuccessEbResponse(ls, pagination) =>
         Stream.emits(ls).covary[F]
           .mapAsyncUnordered(4)(q.enqueue1)
-          .compile.drain
-      case FailedEbResponse(status, details) => L.logError(s"Eventbrite error. Status: $status. Details: $details")
-      case ParsingResponseFailure(failure) => L.logError(failure.getMessage(), failure)
+      case FailedEbResponse(status, details) => Stream.eval(L.logError(s"Eventbrite error. Status: $status. Details: $details"))
+      case ParsingResponseFailure(failure) => Stream.eval(L.logError(failure.getMessage(), failure))
     }
 
-    val init: F[Unit] = for {
-      resp <- callF
+    val init: Stream[F, Unit] = for {
+      resp <- Stream.eval(callF)
       _ <- loop(resp)
     } yield ()
 
--- src/main/scala/er/data/producer/eventbrite/EventbriteDataProducer.scala
+++ src/main/scala/er/data/producer/eventbrite/EventbriteDataProducer.scala
@@ -8,7 +8,7 @@ import er.data.producer.Logger
 import er.data.producer.eventbrite.EventbriteService._
 import fs2.concurrent.Queue
 import io.circe.syntax._
-
+import fs2.Stream
 import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
@@ -23,23 +23,24 @@ object EventbriteDataProducer extends IOApp {
   val readInput: IO[String] = IO(StdIn.readLine())
   val printStr: String => IO[Unit] = s => IO(println(s))
 
-  def program: IO[Unit] = for {
-    _ <- printStr("Enter eventbrite access token")
-    token <- readInput
-    q <- Queue.bounded[IO, EventbriteEvent](100)
+  def program: Stream[IO, Unit] = for {
+    _ <- Stream.eval(printStr("Enter eventbrite access token"))
+    token <- Stream.eval(readInput)
+    q <- Stream.eval(Queue.bounded[IO, EventbriteEvent](100))
     buff = new EventbriteBuffering[IO](q, ioEventBrite.getMunichEvents(token))
-    _ <- buff.start
-    _ <- q.dequeue
-      .mapAsyncUnordered(4)(ev => IO(eventbriteEventToCommonEventData(ev)))
-      .unNone
-      .mapAsyncUnordered(4)(cd => logger.logInfo(cd.asJson.toString))
-      .compile.drain
+    _ <- Stream(
+      buff.start,
+      q.dequeue
+        .mapAsyncUnordered(4)(ev => IO(eventbriteEventToCommonEventData(ev)))
+        .unNone
+        .mapAsyncUnordered(4)(cd => logger.logInfo(cd.asJson.toString))
+    ).parJoin(2)
   } yield ()
 
-  override def run(args: List[String]): IO[ExitCode] = program.attempt.flatMap {
+  override def run(args: List[String]): IO[ExitCode] = program.compile.drain.attempt.flatMap {
     case Left(err) =>
       logger.logError(s"Eventbrite producer error. ${err.getMessage}", err)
         .map(_ => ExitCode.Error)
-    case Right(_) => IO(ExitCode.Success)
+    case Right(_) => logger.logInfo("Program finished successfully.").map(_ => ExitCode.Success)
   }
 }
