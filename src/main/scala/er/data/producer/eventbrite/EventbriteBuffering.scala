c702aeeae67dfd93e7dd7d6d2663b617fc75dff4
Oleksandr Siianko <oleksandr.siyanko@gmail.com>
2018-12-15 09:12:36 +0100

    fetching all events

--- src/main/scala/er/data/producer/eventbrite/EventbriteBuffering.scala
+++ src/main/scala/er/data/producer/eventbrite/EventbriteBuffering.scala
@@ -1,31 +1,45 @@
 package er.data.producer.eventbrite
 
-import cats.effect.ConcurrentEffect
+import cats.effect.{ConcurrentEffect, Timer}
 import er.data.producer.Logger
 import er.data.producer.eventbrite.EventbriteService._
 import fs2.Stream
 import fs2.concurrent.Queue
+import cats.implicits._
+
+import scala.concurrent.duration._
 
 class EventbriteBuffering[F[_]](q: Queue[F, EventbriteEvent], callF: Int => F[EventbriteResponse])
-                               (implicit L: Logger[F], C: ConcurrentEffect[F]) {
+                               (implicit L: Logger[F], C: ConcurrentEffect[F],
+                                T: Timer[F]) {
 
   def start: Stream[F, Unit] = {
 
-    val loop: EventbriteResponse => Stream[F, Unit] = _ match {
-      case SuccessEbResponse(ls, pagination) =>
-        Stream.emits(ls).covary[F]
+    def loop(resp: EventbriteResponse): F[Unit] = resp match {
+      case SuccessEbResponse(ls, pagination) => for {
+        _ <- Stream.emits(ls).covary[F]
           .mapAsyncUnordered(4)(q.enqueue1)
-      case FailedEbResponse(status, details) => Stream.eval(L.logError(s"Eventbrite error. Status: $status. Details: $details"))
-      case ParsingResponseFailure(failure) => Stream.eval(L.logError(failure.getMessage(), failure))
-      case InvalidUrl => Stream.eval(L.logError("Invalid request url."))
+            .compile.drain
+        _ <- if (pagination.moreItems) for {
+          _ <- T.sleep(1.second)
+          _ <- L.logInfo(s"Calling next page: ${pagination.pageNumber + 1}")
+          resp <- callF(pagination.pageNumber + 1)
+          _ <- loop(resp)
+        } yield ()
+        else C.unit
+      } yield ()
+      case FailedEbResponse(status, details) => L.logError(s"Eventbrite error. Status: $status. Details: $details")
+      case ParsingResponseFailure(failure) => L.logError(s"Couldn't parse response. ${failure.getMessage}.", failure)
+      case InvalidUrl => L.logError("Invalid request url.")
     }
 
-    val init: Stream[F, Unit] = for {
-      resp <- Stream.eval(callF(1))
+    val init: F[Unit] = for {
+      _ <- L.logInfo("Calling 1st page.")
+      resp <- callF(1)
       _ <- loop(resp)
     } yield ()
 
-    init
+    Stream.eval(init)
   }
 
 }
